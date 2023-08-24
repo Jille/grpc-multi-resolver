6be3449c9acb06b62c5e330f1fb0f15652b1e2b7
Jille Timmermans <jille@quis.cx>
2023-08-24 21:06:28 +0200

    Upgrade to gRPC v1.57.0
    They removed the fields that were kept for backwards compatibility.

--- go.mod
+++ go.mod
@@ -2,9 +2,9 @@ module github.com/Jille/grpc-multi-resolver
 
 go 1.17
 
-require google.golang.org/grpc v1.55.0
+require google.golang.org/grpc v1.57.0
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- target.go
+++ target.go
@@ -8,7 +8,7 @@ import (
 
 // ParseTarget parses target into resolver.Target URL
 //
-// If target is not a valid URL, it returns {Endpoint: URL: {Path: target}}.
+// If target is not a valid URL, it returns {URL: {Path: target}}.
 func ParseTarget(target string) (ret resolver.Target) {
 	u, err := url.Parse(target)
 	if err != nil {
@@ -16,8 +16,6 @@ func ParseTarget(target string) (ret resolver.Target) {
 	}
 
 	return resolver.Target{
-		Scheme:    u.Scheme,
-		Authority: u.Host,
-		URL:       *u,
+		URL: *u,
 	}
 }
